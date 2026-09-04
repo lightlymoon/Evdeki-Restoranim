ba06026397196152f3ac5c53259ecf8cabede01b
Nuray Guliyeva <gnuray008@gmail.com>
2026-09-04 16:17:01 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyAII5NbG7hFd0lItCsOwnoVdYWXzc5ztyE",
+  apiKey: "",
   authDomain: "evdeki-restoranim-1.firebaseapp.com",
   projectId: "evdeki-restoranim-1",
   storageBucket: "evdeki-restoranim-1.firebasestorage.app",
   messagingSenderId: "238625102318",
-  appId: "1:238625102318:web:449783527cdb6fcc8656ff",
+  appId: "",
   measurementId: "G-EK97FS901E"
 };
 const app = initializeApp(firebaseConfig);
